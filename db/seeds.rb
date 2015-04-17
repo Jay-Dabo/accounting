8a7f22ea3ec4505bf25075aaefc4380a863ad813
Galih Muhammad <galih0muhammad@gmail.com>
2015-04-17 07:26:14 +0700

    revamp seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,34 +3,46 @@
 
 # Temporary admin account
 u = User.new(
-    email: "admin@example.com",
+    email: "galih0muhammad@gmail.com",
     password: "asdasdasd",
     password_confirmation: "asdasdasd",
     first_name: "Galih",
     last_name: "Muhammad",
-    phone_number: "007007007",
+    phone_number: "081399279500",
     admin: true
 )
 # u.skip_confirmation!
 u.save!
 
-# Test user accounts
-(1..50).each do |i|
-  u = User.new(
-      email: "user#{i}@example.com",
-      password: "123123123",
-      password_confirmation: "123123123",
-      first_name: "User",
-      last_name: "#{i}",
-      phone_number: "00700700#{i}"
-  )
-  # u.skip_confirmation!
-  u.save!
+v = User.new(
+    email: "ph.prakosa@gmail.com",
+    password: "rangkul1234",
+    password_confirmation: "rangkul1234",
+    first_name: "Pamungkas",
+    last_name: "Hendra",
+    phone_number: "083866333221",
+    admin: true
+)
+# v.skip_confirmation!
+v.save!
 
-  puts "#{i} test users created..." if (i % 5 == 0)
+# Test user accounts
+# (1..50).each do |i|
+#   u = User.new(
+#       email: "user#{i}@example.com",
+#       password: "123123123",
+#       password_confirmation: "123123123",
+#       first_name: "User",
+#       last_name: "#{i}",
+#       phone_number: "00700700#{i}"
+#   )
+#   # u.skip_confirmation!
+#   u.save!
+
+#   puts "#{i} test users created..." if (i % 5 == 0)
 
 p1 = Plan.new(
-    name: "Icip",
+    name: "Promo",
     duration: 30,
     price: 150000,
     description: "Bayar 150 ribu rupiah setiap bulannya untuk dapat akses"
@@ -42,7 +54,7 @@ p3 = Plan.new(
     duration: 180,
     price: 100000,
     description: "Nikmati tagihan yang lebih murah dengan 
-                  membayar 600 ribu rupiah setiap 6 bulan untuk dapat akses"
+                  membayar 300 ribu rupiah setiap 3 bulan untuk dapat akses"
 )
 p3.save!
 
